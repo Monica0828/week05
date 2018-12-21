23b0c77fbdce3e08610561db834106fa774dc8a7
Monica0828 <danca.monica95@gmail.com>
2018-12-21 15:32:45 +0200

    Added problem with car shopping

---
+++ CarStore/CarStore/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStore
+{
+    class Customer
+    {
+        public Customer(string name)
+        {
+            Name = name;
+            PriceRangeMin = 12000;
+            PriceRangeMax = 15000;
+
+        }
+        public string Name { get; set; }
+
+        public int PriceRangeMin { get; set; }
+
+        public int PriceRangeMax { get; set; }
+
+        public CarPrice MakeDecision(List<CarPrice> list)
+        {
+            foreach (var item in list)
+            {
+                if (list.First().DeliveryTime < list.Last().DeliveryTime && list.First().Price < list.Last().Price)
+                {
+                    return list.First();
+                }
+                else
+
+                if (list.First().DeliveryTime < list.Last().DeliveryTime &&
+                        list.First().Price > list.Last().Price)
+                {
+                    return list.First();
+                }
+                else
+
+                if (list.First().DeliveryTime > list.Last().DeliveryTime &&
+                        list.First().Price < list.Last().Price)
+                {
+                    return list.First();
+                }
+
+                else
+                {
+                    return list.Last();
+                    
+                }                 
+            }
+            return null;
+        }
+    }
+}
---
+++ CarStore/CarStore/IOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarStore
+{
+    interface IOrder
+    {
+        int timeToRepair();
+
+        int timeToWait();
+
+        void cancelOrder();
+    }
+}
